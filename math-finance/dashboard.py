3f90c0ddaa6ea52daa38f07d26fec0612cfafd73
sumunfadil <sumun.fadil@gmail.com>
2025-01-02 01:31:28 +0000

    Added higher order greeks
    TODO:
    - Implied vols + Newton-Raphson

--- math-finance/dashboard.py
+++ math-finance/dashboard.py
@@ -132,9 +132,170 @@ fig.suptitle(f"European {option_type.lower()} option sensitivities", fontsize=16
 plt.tight_layout()
 st.pyplot(fig)
 
+st.write("## First-order Greeks")
+
+epsilons = [BlackScholes(spot_price, strike_price, time_to_maturity, risk_free_rate, \
+                       volatility, option_type.lower()).epsilon\
+                        for risk_free_rate in risk_free_rates]
+lambdas = [BlackScholes(spot_price, strike_price, time_to_maturity, risk_free_rate, \
+                       volatility, option_type.lower()).lambda_\
+                        for spot_price in spot_prices]
+
+fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(10, 5))  
+
+plots = [
+    lambda ax: ax.plot(risk_free_rates, epsilons, linewidth=2, color="black"),
+    lambda ax: ax.plot(spot_prices, lambdas, linewidth=2, color="black"),
+]
+
+xaxis_labels = ["Risk-free rate (r)", "Spot price (S)"]
+greeks = [r"Epsilon ($\epsilon$)", r"Lambda ($\lambda$)"]
+x_values = [risk_free_rate, spot_price]
+y_values = [black_scholes_object.epsilon, black_scholes_object.lambda_]
+
+for i, ax in enumerate(axes.flat):
+        plots[i](ax)
+        ax.set_title(f"{greeks[i]}")
+        ax.set_xlabel(xaxis_labels[i])
+        ax.set_ylabel(greeks[i])
+        ax.grid(True, linestyle="--", alpha=0.9) 
+        ax.scatter(x_values[i], y_values[i], color="black")
+        if i == 1:
+                ax.axvline(x=black_scholes_object.K, color='black', linestyle='--', linewidth=1, zorder=4) 
+
+
+fig.suptitle(f"European {option_type.lower()} option sensitivities", fontsize=16)
+
+plt.tight_layout()
+st.pyplot(fig)
+
+
+
+
 st.write("## Second-order Greeks")
 
+vannas = [BlackScholes(spot_price, strike_price, time_to_maturity, risk_free_rate, \
+                       volatility, option_type.lower()).vanna\
+                        for spot_price in spot_prices]
+charms = [BlackScholes(spot_price, strike_price, time_to_maturity, risk_free_rate, \
+                       volatility, option_type.lower()).charm\
+                        for spot_price in spot_prices]
+vommas = [BlackScholes(spot_price, strike_price, time_to_maturity, risk_free_rate, \
+                       volatility, option_type.lower()).vomma\
+                        for volatility in volatilities]
+veras = [BlackScholes(spot_price, strike_price, time_to_maturity, risk_free_rate, \
+                       volatility, option_type.lower()).vera\
+                        for volatility in volatilities]
+vetas = [BlackScholes(spot_price, strike_price, time_to_maturity, risk_free_rate, \
+                       volatility, option_type.lower()).veta\
+                        for time_to_maturity in time_to_maturities]
+omegas = [BlackScholes(spot_price, strike_price, time_to_maturity, risk_free_rate, \
+                       volatility, option_type.lower()).omega\
+                        for strike_price in strike_prices]
+
+
+fig, axes = plt.subplots(nrows=3, ncols=2, figsize=(10, 12))  
+
+plots = [
+    lambda ax: ax.plot(spot_prices, vannas, linewidth=2, color="black"),
+    lambda ax: ax.plot(spot_prices, charms, linewidth=2, color="black"),
+    lambda ax: ax.plot(volatilities, vommas, linewidth=2, color="black"),
+    lambda ax: ax.plot(volatilities, veras, linewidth=2, color="black"),
+    lambda ax: ax.plot(time_to_maturities, vetas, linewidth=2, color="black"),
+    lambda ax: ax.plot(strike_prices, omegas, linewidth=2, color="black"),
+]
+
+xaxis_labels = ["Spot price (S)", "Spot price (S)", "Volatility ($\sigma$)", "Volatility ($\sigma$)", "Time to maturity (T)", \
+                "Strike prices (K)"]
+greeks = ["Vanna", "Charm", "Vomma", "Vera", "Veta", "Omega"]
+x_values = [spot_price, spot_price, volatility, volatility, time_to_maturity, strike_price]
+y_values = [black_scholes_object.vanna, black_scholes_object.charm, black_scholes_object.vomma, \
+            black_scholes_object.vera, black_scholes_object.veta, black_scholes_object.omega]
+
+for i, ax in enumerate(axes.flat):
+        
+        plots[i](ax)
+        ax.set_title(f"{greeks[i]}")
+        ax.set_xlabel(xaxis_labels[i])
+        ax.set_ylabel(greeks[i])
+        ax.grid(True, linestyle="--", alpha=0.9) 
+        ax.scatter(x_values[i], y_values[i], color="black")
+
+        if i in [0,1]:
+                ax.axvline(x=black_scholes_object.K, color='black', linestyle='--', linewidth=1, zorder=4) 
+ 
+
+fig.suptitle(f"European {option_type.lower()} option sensitivities", fontsize=16)
+
+plt.tight_layout()
+st.pyplot(fig)
+
+
+
 st.write("## Third-order Greeks")
+
+speeds = [BlackScholes(spot_price, strike_price, time_to_maturity, risk_free_rate, \
+                       volatility, option_type.lower()).speed\
+                        for spot_price in spot_prices]
+zommas = [BlackScholes(spot_price, strike_price, time_to_maturity, risk_free_rate, \
+                       volatility, option_type.lower()).zomma\
+                        for volatility in volatilities]
+colors = [BlackScholes(spot_price, strike_price, time_to_maturity, risk_free_rate, \
+                       volatility, option_type.lower()).color\
+                        for time_to_maturity in time_to_maturities]
+ultimas = [BlackScholes(spot_price, strike_price, time_to_maturity, risk_free_rate, \
+                       volatility, option_type.lower()).ultima\
+                        for volatility in volatilities]
+parmicharmas = [BlackScholes(spot_price, strike_price, time_to_maturity, risk_free_rate, \
+                       volatility, option_type.lower()).parmicharma\
+                        for time_to_maturity in time_to_maturities]
+dual_deltas = [BlackScholes(spot_price, strike_price, time_to_maturity, risk_free_rate, \
+                       volatility, option_type.lower()).dual_delta\
+                        for spot_price in spot_prices]
+dual_gammas = [BlackScholes(spot_price, strike_price, time_to_maturity, risk_free_rate, \
+                       volatility, option_type.lower()).dual_gamma\
+                        for spot_price in spot_prices]
+
+
+fig, axes = plt.subplots(nrows=4, ncols=2, figsize=(10, 12))  
+
+plots = [
+    lambda ax: ax.plot(spot_prices, speeds, linewidth=2, color="black"),
+    lambda ax: ax.plot(volatilities, zommas, linewidth=2, color="black"),
+    lambda ax: ax.plot(time_to_maturities, colors, linewidth=2, color="black"),
+    lambda ax: ax.plot(volatilities, ultimas, linewidth=2, color="black"),
+    lambda ax: ax.plot(time_to_maturities, parmicharmas, linewidth=2, color="black"),
+    lambda ax: ax.plot(spot_prices, dual_deltas, linewidth=2, color="black"),
+    lambda ax: ax.plot(spot_prices, dual_gammas, linewidth=2, color="black"),
+]
+
+xaxis_labels = ["Spot price (S)", "Volatility ($\sigma$)", "Time to maturity (T)", "Volatility ($\sigma$)", \
+                "Time to maturity (T)", "Spot price (S)", "Spot price (S)"]
+greeks = ["Speed", "Zomma", "Color", "Ultima", "Parmicharma", "Dual delta", "Dual gamma"]
+x_values = [spot_price, volatility, time_to_maturity, volatility, time_to_maturity, spot_price, spot_price]
+y_values = [black_scholes_object.speed, black_scholes_object.zomma, black_scholes_object.color, \
+            black_scholes_object.ultima, black_scholes_object.parmicharma, black_scholes_object.dual_delta,\
+                black_scholes_object.dual_gamma]
+
+for i, ax in enumerate(axes.flat):
+    if i < len(plots):
+        plots[i](ax)
+        ax.set_title(f"{greeks[i]}")
+        ax.set_xlabel(xaxis_labels[i])
+        ax.set_ylabel(greeks[i])
+        ax.grid(True, linestyle="--", alpha=0.9) 
+        ax.scatter(x_values[i], y_values[i], color="black")
+
+        if i in [0,5,6]:
+                ax.axvline(x=black_scholes_object.K, color='black', linestyle='--', linewidth=1, zorder=4) 
+
+    else:
+        ax.axis("off")  
+
+fig.suptitle(f"European {option_type.lower()} option sensitivities", fontsize=16)
+
+plt.tight_layout()
+st.pyplot(fig)
 
 
 st.write("## Volatility smile")
--- math-finance/model.py
+++ math-finance/model.py
@@ -20,6 +20,10 @@ class BlackScholes:
         self.__d1 = (np.log(self.S/self.K) + (r + 0.5*self.sigma*self.sigma)*self.T)/\
             (self.sigma*np.sqrt(self.T))
         self.__d2 = self.__d1 - self.sigma*np.sqrt(self.T)
+
+        self.__dd1dT2 = (self.r+0.5*self.sigma*self.sigma)*self.T/(self.sigma*np.sqrt(self.T)) -\
+            0.5*self.__d1/self.T
+        self.__dd2dT2 = self.__dd1dT2 - 0.5*self.sigma/np.sqrt(self.T)
         
         if self.type == "call":
             self.price = self.S*norm.cdf(self.__d1) - self.K*np.exp(-self.r*self.T)*norm.cdf(self.__d2)
@@ -32,6 +36,34 @@ class BlackScholes:
                 self.r*self.K*np.exp(-self.r*self.T)*norm.cdf(self.__d2)
             self.rho = self.K*self.T*np.exp(-self.r*self.T)*norm.cdf(self.__d2)
 
+            self.epsilon = -self.S*self.T*norm.cdf(self.__d1)
+            self.lambda_ = self.delta * self.S / self.price 
+
+            self.vanna = -norm.pdf(self.__d1)*self.__d2/self.sigma
+            self.charm = -norm.pdf(self.__d1)*(2*self.r*self.T - self.__d2*self.sigma*np.sqrt(self.T))/\
+                (2*self.T*self.sigma*np.sqrt(self.T))
+            self.vomma = self.vega*self.__d1*self.__d2/self.sigma
+            self.vera = -self.K*self.T*np.exp(-self.r*self.T)*norm.pdf(self.__d2)*self.__d1/self.sigma
+            self.veta = -self.S*norm.pdf(self.__d1)*np.sqrt(self.T)*( \
+                ((self.r*self.__d1)/(self.sigma*np.sqrt(self.T))) - \
+                    ((1+self.__d1*self.__d2)/(2*self.T)) )
+            self.omega = (self.S*self.S*self.gamma)/(self.K*self.K)
+
+            self.speed = - (self.gamma/self.S)*(self.__d1/(self.sigma*np.sqrt(self.T)) + 1)
+            self.zomma = self.gamma*(self.__d1*self.__d2 - 1)/self.sigma
+            self.color = -(norm.pdf(self.__d1)/(2*self.S*self.T*self.sigma*np.sqrt(self.T)))*\
+                (1 + ((2*self.r*self.T - self.__d2*self.sigma*np.sqrt(self.T))/(self.sigma*np.sqrt(self.T))*self.__d1))
+            self.ultima = -(self.vega/(self.sigma*self.sigma))*(self.__d1*self.__d2*(\
+                1-self.__d1*self.__d2) + self.__d1*self.__d1 + self.__d2*self.__d2 )
+            self.parmicharma = -(2*self.r*self.T-self.__d2*self.sigma*np.sqrt(self.T))/\
+                (2*self.T*self.sigma*np.sqrt(self.T))*self.charm + norm.pdf(self.__d1)*\
+                    (2*self.__d2*self.sigma*self.sigma*self.T - self.r*self.sigma*self.T*np.sqrt(self.T)-\
+                     self.sigma*self.sigma*self.T*self.T*self.__dd2dT2)/\
+                        (2*self.T*self.T*self.T*self.sigma*self.sigma)
+            self.dual_delta = -np.exp(-self.r*self.T)*norm.cdf(self.__d2)
+            self.dual_gamma = np.exp(-self.r*self.T)*norm.pdf(self.__d2)/(self.K*self.sigma*np.sqrt(self.T))
+
+
         elif self.type == "put":
             self.price =  self.K*np.exp(-self.r*self.T)*norm.cdf(-self.__d2) - self.S*norm.cdf(-self.__d1)
             self.delta = -norm.cdf(-self.__d1)
@@ -40,3 +72,38 @@ class BlackScholes:
             self.theta = -(self.S*norm.pdf(self.__d1)*self.sigma)/(2*np.sqrt(self.T)) +\
                 self.r*self.K*np.exp(-self.r*self.T)*norm.cdf(-self.__d2)
             self.rho = -self.K*self.T*np.exp(-self.r*self.T)*norm.cdf(-self.__d2)
+
+            self.epsilon = self.S*self.T*norm.cdf(-self.__d1)
+            self.lambda_ = self.delta * self.S / self.price 
+
+            self.vanna = -norm.pdf(self.__d1)*self.__d2/self.sigma
+            self.charm = -norm.pdf(self.__d1)*(2*self.r*self.T - self.__d2*self.sigma*np.sqrt(self.T))/\
+                (2*self.T*self.sigma*np.sqrt(self.T))
+            self.vomma = self.vega*self.__d1*self.__d2/self.sigma
+            self.vera = -self.K*self.T*np.exp(-self.r*self.T)*norm.pdf(self.__d2)*self.__d1/self.sigma
+            self.veta = -self.S*norm.pdf(self.__d1)*np.sqrt(self.T)*( \
+                ((self.r*self.__d1)/(self.sigma*np.sqrt(self.T))) - \
+                    ((1+self.__d1*self.__d2)/(2*self.T)) )
+            self.omega = (self.S*self.S*self.gamma)/(self.K*self.K)
+
+            self.speed = - (self.gamma/self.S)*(self.__d1/(self.sigma*np.sqrt(self.T)) + 1)
+            self.zomma = self.gamma*(self.__d1*self.__d2 - 1)/self.sigma
+            self.color = -(norm.pdf(self.__d1)/(2*self.S*self.T*self.sigma*np.sqrt(self.T)))*\
+                (1 + ((2*self.r*self.T - self.__d2*self.sigma*np.sqrt(self.T))/(self.sigma*np.sqrt(self.T))*self.__d1))
+            self.ultima = -(self.vega/(self.sigma*self.sigma))*(self.__d1*self.__d2*(\
+                1-self.__d1*self.__d2) + self.__d1*self.__d1 + self.__d2*self.__d2 )
+            self.parmicharma = -(2*self.r*self.T-self.__d2*self.sigma*np.sqrt(self.T))/\
+                (2*self.T*self.sigma*np.sqrt(self.T))*self.charm + norm.pdf(self.__d1)*\
+                    (2*self.__d2*self.sigma*self.sigma*self.T - self.r*self.sigma*self.T*np.sqrt(self.T)-\
+                     self.sigma*self.sigma*self.T*self.T*self.__dd2dT2)/\
+                        (2*self.T*self.T*self.T*self.sigma*self.sigma)
+            self.dual_delta = np.exp(-self.r*self.T)*norm.cdf(-self.__d2)
+            self.dual_gamma = np.exp(-self.r*self.T)*norm.pdf(self.__d2)/(self.K*self.sigma*np.sqrt(self.T))
+
+
+            
+            
+            
+
+
+            
